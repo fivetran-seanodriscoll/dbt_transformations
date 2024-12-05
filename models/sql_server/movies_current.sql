9e6000c7e1a8fc6cc944ce7b9c34d3ebb0e5bfdd
fivetran-seanodriscoll <sean.odriscoll@fivetran.com>
2024-12-05 10:25:32 +0000

    Clean up directories

--- models/sql_server/movies_current.sql
+++
@@ -1,3 +0,0 @@
-select *
-from {{source('sql_server_dbo','movie_scd2')}}
-where _fivetran_active = true -- Current Versions of Records Only
--- models/sql_server/movies_historical.sql
+++
@@ -1,3 +0,0 @@
-select *
-from {{source('sql_server_dbo','movie_scd2')}}
-where _fivetran_active = false -- Historical Versions of Records Only
